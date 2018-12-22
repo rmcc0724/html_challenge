4fcb7a2a62014ac245514f3fb30ca24e5eaf5a85
Reid <rmcc0724@gmail.com>
2018-12-22 00:51:23 +0000

    Dice Game_1B

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -7,61 +7,70 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+var scores, roundScore, activePlayer, gamePlaying, twoSixes, winningScore, gameStarted;
+init();
+document.getElementById('displayWinValue').textContent = 'The winning score is ' + winningScore + ".";
 document.querySelector('.btn-roll').addEventListener('click', function() {
+    gameStarted = true;
+    if (gamePlaying) {
 
-    //1. Random Number
+        //1. Random Number
 
-    var dice = Math.floor(Math.random() * 6) + 1;
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    //2. Display the result
+        //2. Display the result
+        twoSixes[1] = twoSixes[0];
+        twoSixes[0] = dice;
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        console.log(twoSixes);
+        console.log(checkTwoSixes());
+        //3. Update the round IF the rolles numner was NOT 1
 
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
 
-    //3. Update the round IF the rolles numner was NOT 1
-
-    if (dice !== 1) {
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        if (checkTwoSixes()) {
+            scores[activePlayer] = 0;
+            document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+            nextPlayer();
+        }
+        else if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }
+        else {
+            //Next Player
+            nextPlayer();
+        }
     }
-    else {
-        //Next Player
-        nextPlayer();
-    }
+
 });
 
-
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    //Add current score to the GLOBAL score
-    scores[activePlayer] += roundScore;
 
-    //Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    //Check if the player won the game
-    if (scores[activePlayer] >= 100) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    if (gamePlaying) {
+        //Add current score to the GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        //Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //Check if the player won the game
+        if (scores[activePlayer] >= winningScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }
+        else {
+            //Next Player
+            twoSixes = [0, 0];
+            nextPlayer();
+        }
     }
-    else {
-        //Next Player
-        nextPlayer();
-    }
-
 });
 
 function nextPlayer() {
@@ -74,3 +83,52 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    winningScore = 100;
+    twoSixes = [0, 0];
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gameStarted = false;
+    gamePlaying = true;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.getElementById('displayWinValue').textContent = "The winning score is " + winningScore;
+    document.getElementById('win-value').value = '';
+}
+
+function checkTwoSixes() {
+    return twoSixes[0] === 6 && twoSixes[1] === 6;
+}
+
+function setWinValue() {
+
+    if (!gameStarted) {
+        winningScore = document.getElementById('win-value').value;
+        if (winningScore < 1 || isNaN(winningScore)) {
+            document.getElementById('displayWinValue').textContent = "The winning score must be a positive number greater than 0!";
+        }
+        else {
+            document.getElementById('displayWinValue').textContent = "The winning score is " + winningScore;
+        }
+    }
+}
+
+///1. Player looses entire score when two 6's in a row are rolled.
+
+///2. Add field property where users can enter the winning score
+
+///3. Add another dice to the game
