009fbe8df313b2def08bc31e7cddff5062ea2daf
Reid <rmcc0724@gmail.com>
2018-12-30 22:42:50 +0000

    Setting up the event listeners

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,29 +1,36 @@
+//Budget COntroller
 var budgetController = (function() {
- 
-  var x = 23;
-  
-  var add = function(a) {
-      return x + a;
-  }
-  
-  return {
-      publicTest: function(b) {
-          return add(b);
-      }
-  };
+
+
 })();
 
-
+//UI Controller
 var UIController = (function() {
-    
+
 })();
 
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
 
-var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z)
-        }
+
+    var ctrlAddItem = function() {
+
+        //1. Get the field input data
+
+        //2. Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget to the UI 
+        console.log('It works');
     }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
