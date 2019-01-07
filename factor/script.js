93377d53f3f505e668fe54de87f34a19da7c43b1
Reid <rmcc0724@gmail.com>
2019-01-07 22:07:12 +0000

    Coding challenge 2

--- factor/script.js
+++ factor/script.js
@@ -2,20 +2,23 @@ var nums1 = [2, 4, 6, 8, 10];
 var nums2 = [2, 3, 4, 5, 6];
 
 function generalizedGCD(num, arr) {
-    var tempHold = [];
-    var newArr = arr.map(value => value);
-    var newTempArr = newArr.reverse();
+    var newArr = arr.map(value => value).reverse();
+    var mod = true;
 
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = newTempArr[i]; j > 0; j--) {
-        if(arr[i]%j!==0)
+    for (var i = newArr[0]; i > 0; i--) {
+        mod = true;
         
-        
+        for (var j = 0; j < newArr.length; j++) {
+            // console.log(newArr[j] + ' ' + i);
+            // console.log(newArr[j]%i);
+            if(newArr[j]%i!== 0) {
+                mod = false;
+            } 
+         }
+         if(mod) {
+             return i;
+         }
 
+        }
     }
-    return newTempArr;
-
-}
-
 console.log(generalizedGCD(5, nums1));
-console.log(generalizedGCD(5, nums2));
