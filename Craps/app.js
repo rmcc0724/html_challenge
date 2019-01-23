13dd29e1b2852bf759b02a79416754a0e3768bd3
Reid <rmcc0724@gmail.com>
2019-01-23 04:14:25 +0000

    Expanding the bank controller

--- Craps/app.js
+++ Craps/app.js
@@ -8,25 +8,42 @@ var bankController = (function() {
         bank: 100,
         point: "Off"
     };
-
     return {
         placeBet: function(bet) {
             if (data.bank > 0) {
                 data.bet = bet;
                 data.bank -= data.bet;
-
                 console.log(data.bank);
             }
             else {
                 console.log("You don't have enough money!");
             }
         },
-
-        getBank: function() {
-            console.log(data.bank);
-        },
         getBet: function() {
             console.log(data.bet);
+        },
+        rollDice: function() {
+            if(data.bet > 0) {
+          data.dice.die1 = Math.floor(Math.random() * 6) + 1;  
+          data.dice.die2 = Math.floor(Math.random() * 6) + 1;  
+          console.log(data.dice.die1);
+          console.log(data.dice.die2);
+            } else { 
+                console.log("You need to place a bet first");
+            }
+        },
+        getDiceTotal: function() {
+            console.log(data.dice.die1 + data.dice.die2);
+        },
+        checkWinner: function() {}
+        
+        ,
+        playerWins: function() {
+            data.bank += data.bet;
+            console.log(data.bank);
+        },
+        getBank: function() {
+            console.log(data.bank);
         }
     };
 })();
