0cb657896ceff11656c3c7ab03db01814a54a78b
Reid <rmcc0724@gmail.com>
2019-01-25 01:32:43 +0000

    Implementing the logic and winning

--- Craps/app.js
+++ Craps/app.js
@@ -23,50 +23,46 @@ var bankController = (function() {
             console.log(data.bet);
         },
         rollDice: function() {
-            if(data.bet > 0) {
-          data.dice.die1 = Math.floor(Math.random() * 6) + 1;  
-          data.dice.die2 = Math.floor(Math.random() * 6) + 1;  
-          console.log(data.dice.die1);
-          console.log(data.dice.die2);
-            } else { 
+            if (data.bet > 0) {
+                data.dice.die1 = Math.floor(Math.random() * 6) + 1;
+                data.dice.die2 = Math.floor(Math.random() * 6) + 1;
+                console.log(data.dice.die1);
+                console.log(data.dice.die2);
+            }
+            else {
                 console.log("You need to place a bet first");
             }
         },
         getDiceTotal: function() {
             return data.dice.die1 + data.dice.die2;
+        },  
+        resetDice: function() {
+            data.dice.die1 = 0, data.dice.die2 = 0;
+            
         },
         checkWinner: function() {
-         
-         //Check to see if a bet was placed and if yes roll the dice if not alert the user to place a bet first
-         data.bet > 0 ? this.rollDice() : console.log("You must first place a bet!!");
-         
-         if(data.bet > 0) {
-         //If point is off do the following
-         data.point === "Off" ? 
-            this.getDiceTotal() === 2 || this.getDiceTotal() === 3 || this.getDiceTotal() === 12 ? this.playerLoses() : 
-            this.getDiceTotal() === 7 || this.getDiceTotal() === 11 ? this.playerWins() : console.log("Roll Again") : console.log("Roll Again");
-            
-        // this.getDiceTotal() === 7 ? this.playerLoses() : (data.point = this.getDiceTotal(), console.log("Roll Again but dont crap out"))
-        //                       : (data.point = this.getDiceTotal(), console.log("Roll Again but dont crap out"));
-          }                        
-        
-        //  console.log("The point is " + this.getPoint());
-        //  console.log("The dice total is " + this.getDiceTotal());
-                //if the dice value is 2,3 or 12 , if true then set bet to 0, do nothing to point and tell the user they crapped out
-                //else if the value is 7 or 11 set add the bet to the bank total, set the bet to 0 , do nothing with point
-                         
-         //Else do the following
-                //if dice total is 7 ? set bet to 0, set the point to 0, tell the user they crapped out
-                //else if dice total is equal to the point ? add the bet to the bank total, tell the user they won , set the bet to 0 , set the point to 0
-                //else display the dice total and tell the user to roll again and attampt to hit the point
+            //Check to see if a bet was placed and if yes roll the dice if not alert the user to place a bet first
+            this.getBet() > 0 ? this.rollDice() : console.log("You must first place a bet!!");
+            if (this.getBet() > 0) {
+                if (this.getPoint() === "Off") {
+                    this.getDiceTotal() === 2 || this.getDiceTotal() === 3 || this.getDiceTotal() === 12 ? this.playerLoses() :
+                        this.getDiceTotal() === 7 || this.getDiceTotal() === 11 ? this.playerWins() : this.setPoint(this.getDiceTotal());
+                }
+                else {
+                    this.getDiceTotal() === 7 ? this.playerLoses() : (this.setPoint(this.getDiceTotal()), console.log("Roll Again but dont crap out"));
+                }
+            }
+             console.log("The point is " + this.getPoint());
+             console.log("The dice total is " + this.getDiceTotal());
+             this.resetDice();
         },
         playerWins: function() {
             data.point = "Off";
-            data.bank += data.bet;
+            data.bank += data.bet*2;
             console.log("Player Wins and has the amount of " + data.bank + " left.");
             console.log(data.bank);
             data.bet = 0;
-        },        
+        },
         playerLoses: function() {
             data.point = "Off";
             console.log("Player Loses and has the amount of " + data.bank + " left.");
@@ -74,9 +70,12 @@ var bankController = (function() {
         },
         getBank: function() {
             return data.bank;
-        }, 
+        },
         getPoint: function() {
             return data.point;
+        },
+        setPoint: function(point) {
+            data.point = point;
         }
     };
 })();
