7f4b07eba9f6d234cbadf5297c9ec872892f8b97
Reid <rmcc0724@gmail.com>
2019-01-25 01:12:32 +0000

    Implementing the logic and getting confused

--- Craps/app.js
+++ Craps/app.js
@@ -33,17 +33,50 @@ var bankController = (function() {
             }
         },
         getDiceTotal: function() {
-            console.log(data.dice.die1 + data.dice.die2);
+            return data.dice.die1 + data.dice.die2;
         },
-        checkWinner: function() {}
+        checkWinner: function() {
+         
+         //Check to see if a bet was placed and if yes roll the dice if not alert the user to place a bet first
+         data.bet > 0 ? this.rollDice() : console.log("You must first place a bet!!");
+         
+         if(data.bet > 0) {
+         //If point is off do the following
+         data.point === "Off" ? 
+            this.getDiceTotal() === 2 || this.getDiceTotal() === 3 || this.getDiceTotal() === 12 ? this.playerLoses() : 
+            this.getDiceTotal() === 7 || this.getDiceTotal() === 11 ? this.playerWins() : console.log("Roll Again") : console.log("Roll Again");
+            
+        // this.getDiceTotal() === 7 ? this.playerLoses() : (data.point = this.getDiceTotal(), console.log("Roll Again but dont crap out"))
+        //                       : (data.point = this.getDiceTotal(), console.log("Roll Again but dont crap out"));
+          }                        
         
-        ,
+        //  console.log("The point is " + this.getPoint());
+        //  console.log("The dice total is " + this.getDiceTotal());
+                //if the dice value is 2,3 or 12 , if true then set bet to 0, do nothing to point and tell the user they crapped out
+                //else if the value is 7 or 11 set add the bet to the bank total, set the bet to 0 , do nothing with point
+                         
+         //Else do the following
+                //if dice total is 7 ? set bet to 0, set the point to 0, tell the user they crapped out
+                //else if dice total is equal to the point ? add the bet to the bank total, tell the user they won , set the bet to 0 , set the point to 0
+                //else display the dice total and tell the user to roll again and attampt to hit the point
+        },
         playerWins: function() {
+            data.point = "Off";
             data.bank += data.bet;
+            console.log("Player Wins and has the amount of " + data.bank + " left.");
             console.log(data.bank);
+            data.bet = 0;
+        },        
+        playerLoses: function() {
+            data.point = "Off";
+            console.log("Player Loses and has the amount of " + data.bank + " left.");
+            data.bet = 0;
         },
         getBank: function() {
-            console.log(data.bank);
+            return data.bank;
+        }, 
+        getPoint: function() {
+            return data.point;
         }
     };
 })();
