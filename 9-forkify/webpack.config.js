6de4cfda5ea9f74534a90818e45e89a6cc825485
Reid <rmcc0724@gmail.com>
2019-02-23 02:31:42 +0000

    Finally done with webpack server

--- 9-forkify/webpack.config.js
+++ 9-forkify/webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
-
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
-   mode: 'development'
+   mode: 'development',
+   devServer: {
+       contentBase: './dist'
+   },
+   plugins: [
+       new HtmlWebPackPlugin({
+           filename: 'index.html',
+           template: './src/index.html'
+       })
+   ]
 };
