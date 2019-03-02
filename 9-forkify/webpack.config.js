05249109431a19b434a763e6c554b06a25358f91
Reid <rmcc0724@gmail.com>
2019-03-02 01:03:49 +0000

    Html Email for mom almost done

--- 9-forkify/webpack.config.js
+++ 9-forkify/webpack.config.js
@@ -1,19 +1,30 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
     },
-   mode: 'development',
-   devServer: {
-       contentBase: './dist'
-   },
-   plugins: [
-       new HtmlWebPackPlugin({
-           filename: 'index.html',
-           template: './src/index.html'
-       })
-   ]
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
