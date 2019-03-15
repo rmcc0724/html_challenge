2591686310a2cd4b44eeb226ecf4d103494b8252
Reid <rmcc0724@gmail.com>
2019-03-15 02:08:53 +0000

    Tricky JS_2

--- tricky.js
+++ tricky.js
@@ -1,1 +1,22 @@
-var A = { x:function() {console.log('x')} y:function() {console.log('y')} z:function() {console.log('z')} }
+var A = {
+    x: function() {
+        console.log('x');
+        return A;
+    },
+    y: function() {
+        console.log('y');
+        return A;
+    },
+    z: function() {
+        console.log('z');
+        return A;
+    }
+};
+A.x().y().z();
+
+
+console.log(0.1 + 0.2);
+
+const x = 'constructor';
+
+console.log(x[x](01));
